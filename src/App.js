91a8fd89c3518a9872202dbbc4110eaf3f4f198f
Maurice <mauriceglenn2@gmail.com>
2023-11-06 12:53:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { auth } from "./firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//<--------------sign up, sign in, sign out---------------->
+// function App() {
+//   const [user, setUser] = useState({})
+//   const [loading, setLoading] = useState(true)
 
+//   //after refreshing the page user stays logged in
+//   useEffect (() => {
+//     //takes time to load, use loading state
+//     onAuthStateChanged(auth, (user) => {
+//       setLoading(false)
+//       console.log(user)
+//       if (user){
+//         setUser(user)
+//       }
+//     })
+//   }, [])
+
+//   function register(){
+//     createUserWithEmailAndPassword(auth, "M@EmailAuthCredential.com", "password")
+//     .then((user) => {
+//       console.log(user)
+//     })
+//     .catch((error) => {
+//      console.log(error)
+//     })
+//   }
+
+//   function login(){
+//     signInWithEmailAndPassword(auth, "M@EmailAuthCredential.com", "password")
+//     .then(({ user }) => {
+//       setUser(user)
+
+//     // Signed in 
+//    console.log(user)
+//   })
+//   .catch((error) =>{
+//     console(error)
+//   })
+// }
+
+//   function logOut (){
+//     signOut(auth)
+//     //set object empty
+//     setUser({})
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <button onClick={register}>Register</button>
+//       <button onClick={login}>Login</button>
+//       <button onClick={logOut}>Log Out</button>
+//       {/* set loading state */}
+//       {loading ?"Loading.." : user.email}
+//       </div>
+//   );
+
+// }
 export default App;
---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//use firebase auth apis
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAO5cRZdw-GgjYM2mSYB0Xwu0X5xheeErI",
+  authDomain: "fir-intro-7badd.firebaseapp.com",
+  projectId: "fir-intro-7badd",
+  storageBucket: "fir-intro-7badd.appspot.com",
+  messagingSenderId: "493953663864",
+  appId: "1:493953663864:web:459ade0bee93d8c9f53e8d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
